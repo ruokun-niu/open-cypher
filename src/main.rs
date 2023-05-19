f86cb886d97bed26b94708462c8acac413acf0f7
Ruokun Niu <ruokun@Ruokuns-MacBook-Pro.local>
2023-05-19 11:02:50 -0700

    wip

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,15 @@
 fn main() {
     use open_cypher::parser::print_pairs;
     use open_cypher::parser::parse;
-    let code = "MATCH (n) WHERE n.name CONTAINS \"s\" RETURN n.name;";
+    // let code = "MATCH (n) WHERE n.name CONTAINS \"s\" RETURN n.name;";
+    let code = "MATCH 
+                         (v:Vehicle)-[:LOCATED_IN]->(:Zone {type:'Parking Lot'}) 
+                         RETURN 
+                         elementId(v) AS id, 
+                         v.make AS make, 
+                         v.model AS model, 
+                         v.color AS color, 
+                         v.plate AS plate";
     // let code = "MATCH (n) WHERE n.name CONTAINS12 RETURN n.name;";
     match parse(code) {
         Ok(tree) => print_pairs(tree),
